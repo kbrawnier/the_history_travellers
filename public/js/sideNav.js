1ebef510e19f427703f696f085fa7ed34225e70c
kbrawnier <38894973+kbrawnier@users.noreply.github.com>
2019-01-23 13:35:30 -0600

    fixing public

--- public/js/sideNav.js
+++ public/js/sideNav.js
@@ -1,11 +1,7 @@
 function openNav() {
-    document.getElementById("collapse-nav").style.width = "250px";
-    document.getElementById("content").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("sideNav").style.width = "250px";
   }
 
   function closeNav(){
-    document.getElementById("collapse-nav").style.width = "0";
-    document.getElementById("content").style.marginLeft ="0";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("sideNav").style.width = "0";
 }
